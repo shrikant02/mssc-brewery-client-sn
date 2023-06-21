5dfb880f7c5ed997466ecde0c90eee21615b7165
shrikant02 <shrikantnavghare2001@gmail.com>
2023-06-21 23:07:51 +0530

    adding get with rest template

---
+++ src/main/java/com/shrikant/web/client/BreweryClient.java
@@ -0,0 +1,26 @@
+package com.shrikant.web.client;
+
+import com.shrikant.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+@Component
+@ConfigurationProperties(value = "mssc.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+    public BeerDto getBeerById(UUID uuid){
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+}
