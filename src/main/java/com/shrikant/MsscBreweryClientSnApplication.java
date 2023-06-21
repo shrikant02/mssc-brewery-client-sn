457dbb5baa4f4c0977cad02748495579b2350db1
shrikant02 <shrikantnavghare2001@gmail.com>
2023-06-21 23:10:01 +0530

    adding GET with rest template

---
+++ src/main/java/com/shrikant/MsscBreweryClientSnApplication.java
@@ -0,0 +1,12 @@
+package com.shrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientSnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryClientSnApplication.class, args);
+	}
+
+}
